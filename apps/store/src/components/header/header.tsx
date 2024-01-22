2858bc2094e2a7f40b77d00e969933e9b9651338
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2024-01-22 11:26:03 -0300

    Header-min-screen estilizada sem ações

--- apps/store/src/components/header/header.tsx
+++ apps/store/src/components/header/header.tsx
@@ -1,16 +1,26 @@
 import { cookies } from 'next/headers'
 import React from 'react'
+import './header.scss'
 
 export default function Header() {
   //const token = cookies().get('access_token')
 
   return (
-    <header className="flex justify-center items-center self-stretch h-14 shadow-md bg-violet-500">
+    <header className="flex justify-center items-center self-stretch h-14 shadow-md bg-primary text-primary-foreground">
       <div className="flex items-center self-stretch flex-1 gap-4 py-2 px-4">
-        <input type="text" placeholder="Pesquisar" />
+        <button className="flex justify-center items-center">
+          <i className="icon-[lucide--search] w-8 h-8"></i>
+        </button>
+        <input
+          type="text"
+          placeholder="Pesquisar"
+          className="font-medium border-none outline-none bg-transparent w-full placeholder:text-gray-300"
+        />
       </div>
-      <div>
-        <i className="icon-[solar--hamburger-menu-broken]"></i>
+      <div className="flex justify-center items-center">
+        <button className="flex justify-center items-center">
+          <i className="icon-[solar--hamburger-menu-broken] w-14 h-14"></i>
+        </button>
       </div>
     </header>
   )
