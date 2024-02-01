7fd41acf03fa453080e1fe14f2ae8669265f9c4b
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2024-01-31 23:43:45 -0300

    Alteração no estilo do popup da sacola

--- apps/store/src/components/header/bag-popup.tsx
+++ apps/store/src/components/header/bag-popup.tsx
@@ -56,6 +56,19 @@ export default function BagPopup() {
     setIsOpen(!isOpen)
   }
 
+  function getTotal() {
+    let total = 0
+
+    listProduct.forEach((product) => {
+      total += product.price
+    })
+
+    return total.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    })
+  }
+
   return (
     <Popover onOpenChange={open}>
       <PopoverTrigger
@@ -65,44 +78,57 @@ export default function BagPopup() {
       >
         <i className="icon-[solar--bag-4-bold] w-8 h-8"></i>
       </PopoverTrigger>
-      <PopoverContent className="flex flex-col justify-between items-center w-60 h-80 text-foreground bg-background rounded shadow-md z-20 pt-1">
+      <PopoverContent className="flex flex-col justify-between items-center w-60 h-72 text-foreground bg-background rounded shadow-md z-20 pt-1">
         <div className="bag-popup-content flex flex-col items-center flex-1 self-stretch overflow-y-scroll mb-1">
           {listProduct &&
             listProduct.map((product) => (
               <div
                 key={product.id}
-                className="flex items-center self-stretch gap-2 py-1 px-2 m-1 shadow-md rounded"
+                className="bag-item flex justify-between items-start self-stretch py-1 mx-2 border-b border-primary"
               >
-                <Image
-                  src={
-                    'https://cdn.discordapp.com/attachments/1175185524433109093/1202074715032658010/Frame_2.png?ex=65cc2251&is=65b9ad51&hm=89b8b351dae21ba9f404f380de2d534b9352796b37a7865e5d64672f95ad9266&'
-                  }
-                  alt={''}
-                  width={56}
-                  height={56}
-                  className="rounded"
-                />
-                <div className="flex flex-col items-start gap-1 self-stretch">
-                  <p className="text-sm font-medium first-letter:uppercase">
-                    {product.name}
-                  </p>
-                  <p className="text-xs capitalize">{product.condition}</p>
-                  <p className="text-xs font-bold text-money-green">
-                    {product.price.toLocaleString('pt-BR', {
-                      style: 'currency',
-                      currency: 'BRL',
-                    })}
-                  </p>
+                <div className="flex gap-2">
+                  <Image
+                    src={
+                      'https://cdn.discordapp.com/attachments/1175185524433109093/1202074715032658010/Frame_2.png?ex=65cc2251&is=65b9ad51&hm=89b8b351dae21ba9f404f380de2d534b9352796b37a7865e5d64672f95ad9266&'
+                    }
+                    alt={''}
+                    width={56}
+                    height={56}
+                    className="rounded"
+                  />
+                  <div className="flex flex-col items-start gap-1 self-stretch">
+                    <p className="text-sm font-medium first-letter:uppercase">
+                      {product.name}
+                    </p>
+                    <p className="text-xs font-bold text-primary">
+                      {product.price.toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                      })}
+                    </p>
+                  </div>
                 </div>
+                {/* TODO: AJUSTE */}
+                <button className="bag-item-remove flex justify-center items-center self-stresh rounded-full border">
+                  <span className="">
+                    <p>Confirma?</p>
+                  </span>
+                  <i className="icon-[solar--close-circle-broken] w-4 h-4 text-gray-400"></i>
+                </button>
+                <button className="bag-item-remove flex justify-center items-center self-stresh p-4">
+                  <i className="icon-[solar--close-circle-broken] w-4 h-4 text-gray-400"></i>
+                </button>
+                {/* AJUSTE */}
               </div>
             ))}
         </div>
-        <div className="flex justify-center items-center self-stretch bg-primary rounded-b">
+        <div className="flex justify-center items-center gap-2 self-stretch rounded-b text-xs p-1">
+          <p className="font-medium">Total: {getTotal()}</p>
           <Link
             href={'/'}
-            className="text-sm font-bold text-background py-1 px-2"
+            className="font-bold py-1 px-2 bg-primary text-background rounded"
           >
-            Acessar Sacola
+            Ver carrinho
           </Link>
         </div>
       </PopoverContent>
