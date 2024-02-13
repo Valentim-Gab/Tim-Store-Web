7abacef1743d10b636aec4df420489f655bb71f6
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2024-02-13 19:34:09 -0300

    Ajustes no popup de categoria na tela 2xl

--- apps/store/src/components/category/category-popup.tsx
+++ apps/store/src/components/category/category-popup.tsx
@@ -30,21 +30,19 @@ export default function CategoryPopup() {
           Todas as categorias
         </p>
       </PopoverTrigger>
-      <PopoverContent className="hidden lg:flex flex-col gap-4 p-4 self-stretch w-[960px] mx-8">
-        <div className="flex justify-between items-center w-full">
+      <PopoverContent className="hidden lg:flex flex-col gap-4 self-stretch w-[960px]" align='start'>
+        <div className="flex justify-between items-center p-4 w-full">
           {categoriesNavigation.map((category) => (
             <div
               key={category.id}
+              data-open={selectedCategory === category.id}
+              className="transition ease-in-out duration-400 data-[open=true]:scale-110"
               onMouseEnter={() => {
                 setSelectedCategory(category.id)
               }}
             >
               <CategoryMiniCard.Root
                 href={category.url}
-                className={twMerge(
-                  'hover:scale-110 transition ease-in-out duration-400',
-                  category.id === selectedCategory ? 'scale-110' : ''
-                )}
               >
                 <CategoryMiniCard.Image src={category.image} />
                 <CategoryMiniCard.Text>{category.name}</CategoryMiniCard.Text>
@@ -52,7 +50,7 @@ export default function CategoryPopup() {
             </div>
           ))}
         </div>
-        <div className="flex justify-between self-stretch gap-8 p-4 w-full">
+        <div className="flex justify-between self-stretch gap-2 p-2 w-full">
           {categoriesNavigation
             .filter(
               (categoryNavigation) => categoryNavigation.id === selectedCategory
@@ -60,11 +58,11 @@ export default function CategoryPopup() {
             .map((categoryNavigation) =>
               categoryNavigation.mainCategories.map((mainCategory, index) => (
                 <div key={index} className="flex flex-col gap-2">
-                  <h3 className="font-semibold text-xl">{mainCategory.name}</h3>
-                  <ul className="flex flex-col gap-2">
+                  <h3 className="font-semibold text-xl p-1">{mainCategory.name}</h3>
+                  <ul className="flex flex-col gap-1">
                     {mainCategory.subCategories.map((subCategory, index) => (
                       <li key={index}>
-                        <Link href={subCategory.url}>{subCategory.name}</Link>
+                        <Link href={subCategory.url} className='hover:text-primary p-1'>{subCategory.name}</Link>
                       </li>
                     ))}
                   </ul>
