6088408de1adf5d3f1919fde8391913c290a04ea
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2024-04-04 19:30:43 -0300

    Adicionar estilo da página de perfil em sm-screen

--- apps/store/src/app/perfil/page.tsx
+++ apps/store/src/app/perfil/page.tsx
@@ -50,8 +50,8 @@ export default async function Perfil() {
 
   return (
     <main className="min-h-screen flex flex-col items-center px-4 py-8">
-      <section className="bg-card flex flex-col items-center gap-2 px-8 py-6 rounded shadow w-full">
-        <div className="flex flex-col items-center justify-center gap-2 py-8 w-full">
+      <section className="bg-card flex flex-col items-center gap-2 p-6 rounded shadow w-full max-w-[440px] sm:max-w-[520px] sm:p-8 sm:gap-4">
+        <div className="flex flex-col items-center justify-center gap-2 py-8 w-full sm:gap-4">
           <figure className="flex justify-center items-center p-1 rounded-full border-primary border-2">
             {session.user.profile_image ? (
               <Image
@@ -59,17 +59,17 @@ export default async function Perfil() {
                 width={96}
                 height={96}
                 alt="Imagem do usuário"
-                className="rounded-full w-[96px] h-[96px]"
+                className="rounded-full w-[96px] h-[96px] sm:w-[128px] sm:h-[128px]"
               />
             ) : (
-              <i className="icon-[solar--user-bold] text-primary w-[96px] h-[96px]"></i>
+              <i className="icon-[solar--user-bold] text-primary w-[96px] h-[96px] sm:w-[128px] sm:h-[128px]"></i>
             )}
           </figure>
-          <div className="flex flex-col items-center gap-1 py-1 text-center w-full">
-            <h2 className="text-xl font-bold">{session.user.name}</h2>
+          <div className="flex flex-col items-center gap-1 py-1 text-center w-full sm:gap-2">
+            <h2 className="text-xl font-bold sm:text-2xl">{session.user.name}</h2>
             <span className="flex justify-center items-start self-stretch gap-1 w-full">
-              <i className="icon-[solar--letter-bold] min-w-[16px] min-h-[16px]"></i>
-              <p className="text-xs truncate">{session.user.email}</p>
+              <i className="icon-[solar--letter-bold] min-w-[16px] min-h-[16px] sm:min-w-[20px] sm:min-h-[20px]"></i>
+              <p className="text-xs truncate sm:text-sm">{session.user.email}</p>
             </span>
           </div>
         </div>
@@ -81,11 +81,11 @@ export default async function Perfil() {
                 href={item.href}
                 className="flex justify-between items-center py-1"
               >
-                <span className="flex items-center justify-center gap-2">
+                <span className="flex items-center justify-center gap-2 sm:gap-4">
                   <i
                     className={`${item.icon} w-[32px] h-[32px] text-primary`}
                   ></i>
-                  <p className="text-sm font-medium">{item.text}</p>
+                  <p className="text-sm font-medium sm:text-base">{item.text}</p>
                 </span>
                 <span className="flex items-center justify-center">
                   <i className="icon-[solar--alt-arrow-right-broken] w-[20px] h-[20px]"></i>
